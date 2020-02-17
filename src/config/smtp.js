89d1a468130b476aeddf50dd7912c5bb8c1aa34e
“Iran <iranjunior94@gmail.com>
2020-02-17 14:46:39 -0300

    hotfix(smtp): configuration

--- src/config/smtp.js
+++ src/config/smtp.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const smtpTransport = () => nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
